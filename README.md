7283c114cdec5ff6f30607c573d1e6ec2e5f4c0e
Caliza <rances.rodriguez@gmail.com>
2022-12-22 15:42:28 -0800

    clean version of code with readMe

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Password Generator Starter Code
+
+## Button that any user can create a random password with a length of 8 to 128 characters.
+
+## Uses strings, arrays, for and while loops to create array.
+
+## User instruction:click on Generate Password button and select characters to be contained in password and choose a password length between 8-128 characters. 
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Assignment code here
+// Intiates variables and arrays to be passed through password
 var lowercase = "abcdefghijklmnopqrstuvwxyz";
 var uppercase = lowercase.toUpperCase();
 var numbers = "1234567890";
@@ -10,96 +10,61 @@ var specialcharArr = specialchar.split('');
 var passwordString = '';
 var passwordCharArr = [];
 
-//console.log(lowercaseArr);
-// Get references to the #generate element
+// Set array to generate random number
 var generateBtn = document.querySelector("#generate");
 function randChar(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
 
+//Prompts asking user for selected characters to be used in password.
 function generatePassword() {
-  //console.log("you clicked the button");
-
   var lowercasePrompt = window.confirm("Set password to contain lowercase letter?");
   var uppercasePrompt = window.confirm("Set password to contain uppercase letter?");
   var numbersPrompt = window.confirm("Set password to contain number?");
   var specialcharPrompt = window.confirm("Would you like your password to contain special characters?");
   var  userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
-  while (userPasswordLength < 8 || userPasswordLength > 128) {
-    alert("Password length must be between 8 and 128");
-    userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
-  }
 
+//While loop used to verify that user sets password within given range.
+while (userPasswordLength < 8 || userPasswordLength > 128) {
+  alert("Password length must be between 8 and 128");
+  userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
+}
 
-  // if (userPasswordLength >= 8 && userPasswordLength <= 128) {
-    for (let index = passwordString.length; index < Number(userPasswordLength); index++) {
-      //passwordString += randChar(passwordCharArr);
-      if (lowercasePrompt) {
-      // passwordCharArr.concat(lowercaseArr);
-       passwordString += randChar(lowercaseArr);
-     }
-     console.log('password string', passwordString);
+//Passes seleceted criteria by user into passwordString.
+for (let index = passwordString.length; index < Number(userPasswordLength); index++) {
+      
+  if (lowercasePrompt) {
+    passwordString += randChar(lowercaseArr);
+    }
+    //console.log('password string', passwordString);
    
-     if (uppercasePrompt) {
-      // passwordCharArr.concat(uppercaseArr);
-       passwordString += randChar(uppercaseArr);
-     }
+  if (uppercasePrompt) {
+    passwordString += randChar(uppercaseArr);
+    }
    
-     if (numbersPrompt) {
-       //passwordCharArr.concat(numbersArr);
-       passwordString += randChar(numbersArr);
-     }
+  if (numbersPrompt) {
+    passwordString += randChar(numbersArr);
+    }
    
-     if (specialcharPrompt) {
-       //passwordCharArr.concat(specialcharArr);
-       passwordString += randChar(specialcharArr);
-     }
+  if (specialcharPrompt) {
+    passwordString += randChar(specialcharArr);
+    }
      console.log('password string after', passwordString);
-   }  
-  //}
-  // else {
-  //   alert("Password length must be between 8 and 128");
-  // }
-  
-//   for (let index = passwordString.length; index < Number(userPasswordLength); index++) {
-//    //passwordString += randChar(passwordCharArr);
-//    if (lowercasePrompt) {
-//    // passwordCharArr.concat(lowercaseArr);
-//     passwordString += randChar(lowercaseArr);
-//   }
-//   console.log('password string', passwordString);
+  }  
 
-//   if (uppercasePrompt) {
-//    // passwordCharArr.concat(uppercaseArr);
-//     passwordString += randChar(uppercaseArr);
-//   }
-
-//   if (numbersPrompt) {
-//     //passwordCharArr.concat(numbersArr);
-//     passwordString += randChar(numbersArr);
-//   }
-
-//   if (specialcharPrompt) {
-//     //passwordCharArr.concat(specialcharArr);
-//     passwordString += randChar(specialcharArr);
-//   }
-//   console.log('password string after', passwordString);
-// }
+//Returns password to given length set by user.
 return passwordString.slice(0,userPasswordLength-passwordString.length);
 
-  //console.log(passwordString);
-  //console.log(passwordCharArr);
 }
   // Write password to the #password input
-  function writePassword() {
-    var password = generatePassword();
-    var passwordText = document.querySelector("#password");
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
 
-    passwordText.value = password;
+}
 
-  }
-
-  // Add event listener to generate button
-  generateBtn.addEventListener("click", writePassword);
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
