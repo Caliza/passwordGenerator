0023d8b04e1e4cdbed2386d039a913126ff25002
Caliza <rances.rodriguez@gmail.com>
2023-01-13 18:24:49 -0800

    Redacted code so that app works under all number of character types that are selected. Edited README and added picture of app

--- README.md
+++ README.md
@@ -1,7 +1,18 @@
-# Password Generator Starter Code
+# Password Generator
 
-## Button that any user can create a random password with a length of 8 to 128 characters.
+## Description
 
-## Uses strings, arrays, for and while loops to create array.
+When Generate Password button os clicked, user is prompted to select what type of characters the would like to be included in their password: lowerCase letter, upperCase letters, special characters, and/or number. After selceting which type of characters they would like to use in their password, they must select a password length between 8 and 128 charachters.
 
-## User instruction:click on Generate Password button and select characters to be contained in password and choose a password length between 8-128 characters. 
+- Create an app that a user could use to create a secure password.
+- To provide an app for users looking for more options when seeking an app that provides this service.
+- Generates a strong/secure password.
+- Learned how to use a for loop, strings, arrays, for and while loop to create array.
+
+## Usage
+
+https://caliza.github.io/javascriptQuiz/
+
+![PasswordGenerator!](passwordGenerator.jpg)
+
+## User instruction: click on Generate Password button and select characters to be included in password. Then choose a password length between 8-128 characters. 
--- script.js
+++ script.js
@@ -12,51 +12,56 @@ var passwordCharArr = [];
 
 // Set array to generate random number
 var generateBtn = document.querySelector("#generate");
+// Randomly selcts item from array
 function randChar(arr) {
   return arr[Math.floor(Math.random() * arr.length)];
 }
-
+// Concatanates old Array into new array ("expands")
+function expandArr(originalArr, extendArr) {
+  return originalArr.concat(extendArr)
+}
 //Prompts asking user for selected characters to be used in password.
 function generatePassword() {
   var lowercasePrompt = window.confirm("Set password to contain lowercase letter?");
   var uppercasePrompt = window.confirm("Set password to contain uppercase letter?");
   var numbersPrompt = window.confirm("Set password to contain number?");
   var specialcharPrompt = window.confirm("Would you like your password to contain special characters?");
-  var  userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
+  var userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
 
-//While loop used to verify that user sets password within given range.
-while (userPasswordLength < 8 || userPasswordLength > 128) {
-  alert("Password length must be between 8 and 128");
-  userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
-}
-
-//Passes seleceted criteria by user into passwordString.
-for (let index = passwordString.length; index < Number(userPasswordLength); index++) {
-      
+  //While loop used to verify that user sets password within given range.
+  while (userPasswordLength < 8 || userPasswordLength > 128) {
+    alert("Password length must be between 8 and 128");
+    userPasswordLength = Number(prompt("Chose a password between 8 and 128"))
+  }
   if (lowercasePrompt) {
-    passwordString += randChar(lowercaseArr);
-    }
-    //console.log('password string', passwordString);
-   
+    passwordCharArr = expandArr(passwordCharArr, lowercaseArr);
+  }
   if (uppercasePrompt) {
-    passwordString += randChar(uppercaseArr);
-    }
-   
+    passwordCharArr = expandArr(passwordCharArr, uppercaseArr);
+  }
+
   if (numbersPrompt) {
-    passwordString += randChar(numbersArr);
-    }
-   
+    passwordCharArr = expandArr(passwordCharArr, numbersArr);
+  }
+
   if (specialcharPrompt) {
-    passwordString += randChar(specialcharArr);
-    }
-     console.log('password string after', passwordString);
-  }  
-
-//Returns password to given length set by user.
-return passwordString.slice(0,userPasswordLength-passwordString.length);
-
+    passwordCharArr = expandArr(passwordCharArr, specialcharArr);
+  }
+  if (passwordCharArr.length === 0) {
+    alert("You must select one character type")
+    return ""
+  }
+  console.log('password string after', passwordCharArr);
+  for (let index = 0; index < Number(userPasswordLength); index++) {
+    passwordString += randChar(passwordCharArr)
+  }
+  //Returns password to given length set by user.
+  // return passwordString.slice(0, userPasswordLength - passwordString.length);
+  return passwordString
 }
-  // Write password to the #password input
+//Passes seleceted criteria by user into passwordString.
+
+// Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
